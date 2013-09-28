81239b303935cbfe5bd3f30f1f8ba023c464924d
billtang <billtang@gmail.com>
2013-09-28 14:59:55 -0700

    change to write csv and put U when recipient party is empty

--- getdata.py
+++ getdata.py
@@ -2,6 +2,7 @@
 import csv
 import sys
 def main():
+    csvwriter = csv.writer(sys.stdout,delimiter=',', quotechar='"')
     csvreader = csv.reader(sys.stdin, delimiter=',', quotechar='"')
     for row in csvreader:
         # field: # 8 is contribution amount, 9 is date, 19 is contributor zipcode, 
@@ -10,7 +11,10 @@ def main():
             myzip = 0
             try: myzip = int(row[19])
             except: pass
-            if myzip >= 48005 and myzip <= 48462: print row[8],row[9],row[19],row[27]
+            if myzip >= 48005 and myzip <= 48462: 
+                myparty = 'U'
+                if row[27]!='' and len(row[27])>0: myparty = row[27]
+                csvwriter.writerow([row[8],row[9][:4],row[19],myparty])
 
 if __name__ == "__main__":
     main()
